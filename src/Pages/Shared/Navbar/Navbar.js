abee87fcbf23a7134ca644e7e940331a3c3a1323
imrans09 <mohammadimrans09t@gmail.com>
2023-05-11 11:54:17 +0600

    changed name

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -68,7 +68,7 @@ const Navbar = () => {
             </ul>
           </div>
           <Link to="/" className="btn btn-ghost normal-case text-3xl font-semibold">
-            Doctors Portal
+            Teethnic
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
